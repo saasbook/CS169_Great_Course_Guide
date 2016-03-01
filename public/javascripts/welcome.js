3f9ffd5559b3fa7b52b8b670f7266ebce59ba4b0
Monu <prankum2100@berkeley.edu>
2016-02-29 23:18:28 -0800

    javascript, clean up, autocomplete

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -10,16 +10,17 @@ function validateEmail(email) {
 }
 
 $(function () {
+  $('#selected-classes').addClass("hide")
   $("#continue").click(function() {
 
     first_name = $('#first_name').val();
     last_name = $('#last_name').val();
     email = $('#email').val();
 
-    if (!validateEmail(email)) {
-      alert("Oops", "Please enter a valid email.", "error")
-      return
-    }
+    // if (!validateEmail(email)) {
+    //   alert("Oops", "Please enter a valid email.", "error")
+    //   return
+    // }
 
     if (first_name == "" || last_name == "" || email == "") {
       alert("Oops", "Please fill-in all fields.", "error");
@@ -29,22 +30,23 @@ $(function () {
     $('#details-form').removeClass("slideInUp").addClass("fadeOutLeft")
     setTimeout(function() {
       $('#details-form').addClass("hide")
-      $('#class-form').addClass("animated fadeInRight").removeClass("hide");
+      $('#class-form').removeClass("fadeOutRight").addClass("animated fadeInRight").removeClass("hide");
     }, 400);
   });
 
   $('#addClass').click(function() {
+    $('#selected-classes').removeClass("hide")
     var item = $('#class-search');
     value = item.val();
+
+    if (value === "") {
+      alert("Oops", "Please select a class.", "error");
+      return
+    }
 
     if ($.inArray(value, all_classes) == -1) {
       alert("Oops", "That isn't a valid class.", "error");
       return;
-    }
-
-    if (value == "") {
-      alert("Oops", "Please select a class.", "error");
-      return
     }
 
     if ($.inArray(value, selected_classes) !== -1) {
@@ -59,14 +61,37 @@ $(function () {
       list.append("<li class='collection-item'>" + value + "</li>");
       class_selections.append("<option value='" + value + "' selected>" + value + "</option>");
       selected_classes.push(value);
+      var i = all_classes.indexOf(value);
+      if(i != -1) {
+        all_classes.splice(i, 1);
+      }
     } else {
       alert("");
     }
+
+  });
+
+  $('#class-search').keypress(function (e) {
+   var key = e.which;
+   if(key == 13)  // the enter key code
+    {
+      $('#addClass').click();
+      return false;  
+    }
+  }); 
+
+  $('#back').click(function() {
+    $('#class-form').addClass("fadeOutRight")
+    setTimeout(function() {
+      $('#class-form').addClass("hide")
+      $('#details-form').removeClass("fadeOutLeft").addClass("animated fadeInLeft").removeClass("hide");
+    }, 400);
   });
 
   var input = document.getElementById("class-search");
   var all_classes = ["EE16A", "EE16B", "CS61A", "CS61B", "CS61C", "CS70", "CS169", "CS188"]
   new Awesomplete(input, {
-    list: all_classes
+    list: all_classes,
+    autoFirst: true
   });
 });
