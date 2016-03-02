2ae7295c111a0107b8cb0abedf08f1e61ef9b4ca
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-03-02 07:52:32 -0800

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -6,12 +7,23 @@ class ApplicationController < ActionController::Base
   # Filter Needed for CalNet Login
   before_filter CASClient::Frameworks::Rails::Filter
   before_action :require_info, :except => [:welcome, :all_courses, :create]
+  before_action :get_info
 
   def require_info
     user_exists = User.find_by(uid: session[:cas_user]) != nil
     if not user_exists
       redirect_to "/welcome" and return
     end
+  end
+
+  def get_info
+    @user = User.find_by(uid: session[:cas_user])
+    @user_exists = @user != nil
+    if @user_exists
+      @user_courses = @user.courses
+    end
+    @all_courses = Course.all_courses
+    @all_profs = Professor.all_profs
   end
 
   def logout
@@ -21,55 +33,42 @@ class ApplicationController < ActionController::Base
 
   # Displays a page to allow the user to enter information
   def welcome
-    user_exists = User.find_by(uid: session[:cas_user]) != nil
-    if user_exists
-      redirect_to "/user" and return
-    end
+    redirect_to "/user" and return if @user_exists
+
     render "welcome", layout: false and return
   end
 
   def classes
-    @all_courses = Course.all_courses
   end
 
   def professors
-    @all_profs = Professor.all_profs
   end
 
   def all_courses
-    render :text => Course.all_courses.to_json
-    # respond_to do |format|
-    #   msg = {:status => "ok", :message => "Success!", :courses =>  Course.all_courses}
-    #   format.json  {render :json => msg} # don't do msg.to_json
-    # end
+    render :text => @all_courses.to_json
   end
 
   def create
-    user = User.create(first_name: params[:first_name],
+    # TODO: Need to validate params
+    @user = User.create(first_name: params[:first_name],
                         last_name: params[:last_name], email: params[:email],
                                                         uid: session[:cas_user])
     if params[:class_select] != nil
-      params[:class_select].each do |course| 
+      params[:class_select].each do |course|
         index = course.index(":")
         course_number = course[0..(index-1)]
         title = course[(index + 2)..(course.length - 1)]
-        user.courses.create(title: title, course_number: course_number) 
+        @user.courses.create(title: title, course_number: course_number)
       end
     end
 
     redirect_to "/user" and return
   end
 
-  # Displays the homepage for the user
   def index
-    @user = User.find_by(uid: session[:cas_user])
-    @user_courses = @user.courses
   end
 
   def edit
-    @user = User.find_by(uid: session[:cas_user])
-    @user_courses = @user.courses
-    @all_courses = Course.all_courses
     @user_classNames = []
     @user_courses.each do |course|
       @user_classNames << course.course_number
@@ -77,18 +76,18 @@ class ApplicationController < ActionController::Base
   end
 
   def update
-    @user = User.find_by(uid: session[:cas_user])
-    @user.courses.destroy_all()
+    @user.courses.destroy_all
+
     if params[:classes] != nil
-      params[:classes].each_key do |course| 
+      params[:classes].each_key do |course|
         index = course.index(":")
         course_number = course[0..(index-1)]
         title = course[(index + 2)..(course.length - 1)]
-        @user.courses.create(title: title, course_number: course_number) 
+        @user.courses.create(title: title, course_number: course_number)
       end
     end
+
     redirect_to "/user" and return
   end
-
 
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,12 @@
 class LoginController < ApplicationController
+
   skip_before_filter CASClient::Frameworks::Rails::Filter
   skip_before_action :require_info
+
   def index
     user_exists = User.find_by(uid: session[:cas_user]) != nil
-    if user_exists
-      redirect_to "/user" and return
-    end
+    redirect_to "/user" and return if user_exists
     render "../views/layouts/login" and return
   end
+
 end
