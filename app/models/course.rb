90df07f7d217c628a80a0199c858eff582117353
A412 <max.mf.feng@berkeley.edu>
2016-03-03 23:24:57 -0800

    compute prereqs

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,12 @@
 class Course < ActiveRecord::Base
     has_many :prereqs
+
+    def compute_prereqs_given_user user
+        prereqs = []
+        self.prereqs.each {|prereq|
+            if not user.user_courses.include? prereq
+                prereqs << prereq.number
+            end}
+        prereqs
+    end
 end
