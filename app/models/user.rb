c3c033f6d7921de643eb538b105e939bdef46675
A412 <max.mf.feng@berkeley.edu>
2016-03-03 23:29:16 -0800

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
     has_many :user_courses
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :uid, presence: true, uniqueness: true
 end
