50e49251554c36ea34439bdf7bff38d617fcf213
Monu <prankum2100@berkeley.edu>
2016-04-12 15:07:39 -0700

    done with graphs for now

--- public/javascripts/prof_chart.js
+++ public/javascripts/prof_chart.js
@@ -3,18 +3,20 @@ var data = {
     datasets: [
         {
             label: "Class Ratings",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
+            pointHighlightStroke: "rgba(151,187,205,1)",
             data: terms_and_ratings_j[1]
         },
     ]
 };
 // Get the context of the canvas element we want to select
-var graph= document.getElementById("graph").getContext("2d");
-new Chart(graph).Line(data);
+var graph = document.getElementById("graph").getContext("2d");
+new Chart(graph).Line(data, {
+    scaleGridLineWidth: 1.5
+});
 
 
