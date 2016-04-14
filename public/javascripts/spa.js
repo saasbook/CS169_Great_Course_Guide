23c76650470c571e175455bdd97e9c0e2ec91801
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-04-13 18:45:50 -0700

    Fixed back button issue

--- public/javascripts/spa.js
+++ public/javascripts/spa.js
@@ -1,4 +1,10 @@
 $(function() {
+
+  $(window).bind("pageshow", function(event) {
+    if (event.originalEvent.persisted) {
+      $('.mainContent').removeClass('fadeOut').addClass('fadeIn')
+    }
+  });
   $('.mainContent').addClass('fadeIn');
 
   $(window).on('beforeunload', function() {
