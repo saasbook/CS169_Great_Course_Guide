903a2b1b3c26f8624cefd567296f576f2ca14d31
Monu <prankum2100@berkeley.edu>
2016-04-16 11:35:13 -0700

    pushing graphs

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -8,7 +8,8 @@ class ProfessorsController < ApplicationController
     @prof = Professor.find(params[:id])
     @prof_courses = @prof.courses.order(rating: :desc)
     @uniq_prof_courses = @prof.unique_courses
-    @terms_and_ratings = @prof.chart_info #already ordered
+    @terms_and_ratings = @prof.chart_info 
+    #already ordered (term-wise) (data for graphs)
   end
 
   def distinguished
