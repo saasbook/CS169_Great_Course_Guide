84f398dd55fb8349e8299106c086a91b0bd7fc7d
Anish Balaji <balaji.anish8@gmail.com>
2016-04-19 10:42:35 -0700

    UI fixes

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -8,55 +8,8 @@ $(function() {
     var split = this.id.split("-");
     $("#" + split[0] + "-taken").prop('checked', false);
   });
-  var filters = {"cs": $("#cs"), "ee": $("#ee"), "upper": $("#upper"), "lower": $("#lower"), "grad": $("#grad")}
-  $("#filters").change(function() {
-    $("tr").hide()
-  	var cs = filters["cs"].is(':checked');
-  	var lower = filters["lower"].is(':checked');
-  	var ee = filters["ee"].is(':checked');
-  	var upper = filters["upper"].is(':checked');
-    var grad = filters["grad"].is(':checked');
-  	if (cs) {
-  		if (lower) {
-  			$('.CS_LOWER_DIV').show();
-  		}
-  		if (upper) {
-  			$('.CS_UPPER_DIV').show();
-  		}
-      if (grad) {
-        $('.CS_GRAD').show();
-      }
-  	}
-  	if (ee) {
-  		if (lower) {
-  			$('.EE_LOWER_DIV').show();
-  		}
-  		if (upper) {
-  			$('.EE_UPPER_DIV').show();
-  		}
-      if (grad) {
-        $('.EE_GRAD').show();
-      }
-  	}
-  });
-
-  // Fixed Scrolling
-  scrollDivWithPage = function() {
-    if ($(window).width() <= 950) {
-      $('#filters').removeAttr('style');
-      $('#selections').removeAttr('style');
-    } else {
-      var containerWidth = $('.container').width()
-      $('#filters').css({'position': 'fixed', 'top': '64px', 'width': 180});
-      var filterWidth = $('#filters').width()
-      $('#selections').css({'position': 'absolute', 'top': '64px', 'margin-left': (filterWidth + 10).toString() + 'px', 'width': containerWidth - filterWidth - 10});
-    }
-  }
-  $(window).resize(scrollDivWithPage);
-  scrollDivWithPage();
 
   $('#saveInFilter').click(function() {
     $('#save').click();
   });
-
 });
