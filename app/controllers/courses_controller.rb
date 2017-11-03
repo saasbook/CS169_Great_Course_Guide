9f61a69e401a53376f4287a7eb14c6337ab0da6d
Marcus Lee <marcuslee143@berkeley.edu>
2017-11-03 20:40:58 +0000

    Refactored filtering code from the view into the controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -28,6 +28,21 @@ class CoursesController < ApplicationController
 
   def schedule
     @recommended_EECS_courses = @user.recommended_EECS_courses
+    
+    # for filtering out "Best Alternative Courses"
+    # who's ratings are lower than "Courses You're Interested In"
+    # the overall rating for the professors teaching a course
+    # is stored in the [2] index of a course;
+    # [0] and [1] contain course and professor metadata respectively
+    if not @recommended_EECS_courses[:possible_fall].blank?
+      min_fall_recommended_rating = @recommended_EECS_courses[:possible_fall].min_by {|x| x[2]}[2]
+      @recommended_EECS_courses[:backup_fall] = @recommended_EECS_courses[:backup_fall].select{|a| a[2] > min_fall_recommended_rating}
+    end
+    if not @recommended_EECS_courses[:possible_spring].blank?
+      min_spring_recommended_rating = @recommended_EECS_courses[:possible_spring].min_by {|x| x[2]}[2]
+      @recommended_EECS_courses[:backup_spring] = @recommended_EECS_courses[:backup_spring].select{|a| a[2] > min_spring_recommended_rating}
+    end
+    
     @fall_length = @recommended_EECS_courses[:possible_fall].length + @recommended_EECS_courses[:backup_fall].length
     @spring_length = @recommended_EECS_courses[:possible_spring].length + @recommended_EECS_courses[:backup_spring].length
     @recommended_breadth_courses = @user.recommended_breadth_courses
