46c63c00c5c004da8cc12f645dda0a3cb44c450d
dgaoster <davidgao96@yahoo.com>
2017-12-01 16:16:18 -0800

    deleted an unused function

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,7 +77,6 @@ class User < ActiveRecord::Base
           semester[:backup_courses] << course_number
         end
       end
-      #byebug
       get_course_data(semester[:possible_courses], semester[:courses])
       get_course_data(semester[:backup_courses], semester[:courses])
       
@@ -86,7 +85,6 @@ class User < ActiveRecord::Base
       if not semester[:possible_courses].blank? and not ignore_flag
         min_interested_rating = semester[:possible_courses].min_by {|x| x[2]}[2]  
         min_interested_rating = min_interested_rating.min
-        #byebug
         semester[:backup_courses] = semester[:backup_courses].select{|a| a[2][0] > min_interested_rating}
       end
     end
@@ -110,30 +108,10 @@ class User < ActiveRecord::Base
     return distinguished_fall_breadth_courses
   end
 
-  #protected
-  def get_average_rating_of_professors(professors)
-    total = 0
-    num_professors = 0
-    professors.each do |name|
-      if name == "TBA"
-        next
-      end
-      professor = Professor.find_by(name: name)
-      if professor
-        if professor.rating and professor.rating != "*"
-          total += professor.rating
-          num_professors += 1
-        end
-      end
-    end
-    return num_professors > 0 ? (total / num_professors).round(2) : "*"
-  end
-
   def get_course_data(course_list, term_list)
     course_list.map! { |course_number| [Course.find_by(number: course_number),
               term_list[course_number].split(";")]}
               #get_average_rating_of_professors(term_list[course_number].split(";"))] }
-    #byebug
     course_list.each do |data|
       new_professors = []
       prof_ratings = []
